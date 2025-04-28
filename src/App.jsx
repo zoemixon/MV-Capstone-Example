2c81be8115f4e2989b38d797d68de923553ac903
zoe.mix <zmmix2468@gmail.com>
2025-04-28 12:54:36 -0400

    Pull request consolidation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import { MoleculeViewer } from 'molecular-visualization-capstone'; 
-import { FileParser } from 'molecular-visualization-capstone';
-// import { UIPanelsOnly } from 'molecular-visualization-capstone';
-import { UploadButton } from 'molecular-visualization-capstone';
+import { MoleculeViewer, FileParser, UploadButton } from 'molecular-visualization-capstone';
 
 const App = () => {
   const [files, setFiles] = useState([]);
   const [molecules, setMolecules] = useState([]);
+  const [scene, setScene] = useState(null);
 
   const handleDeleteMolecule = (idx) => {
     setMolecules(prev => prev.filter((_, i) => i !== idx));
@@ -15,10 +13,11 @@ const App = () => {
   return (
     <div>
       <UploadButton setFiles={setFiles} />
-      <FileParser files={files} onParsed={setMolecules} />
+      <FileParser files={files} onParsed={setMolecules} scene={scene} />
       <MoleculeViewer
         molecules={molecules}
         onDeleteMolecule={handleDeleteMolecule}
+        onSceneReady={setScene}
       />
     </div>
   );
